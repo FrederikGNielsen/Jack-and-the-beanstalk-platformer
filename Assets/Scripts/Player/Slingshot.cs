ace0de293924576b44215e4eeeedaa25b2e7203e
Slowri <71567638+Slowri@users.noreply.github.com>
2022-10-31 18:04:05 +0100

    test123

--- Assets/Scripts/Player/Slingshot.cs
+++ Assets/Scripts/Player/Slingshot.cs
@@ -22,7 +22,7 @@ public class Slingshot : MonoBehaviour
 
     //UI
     public TMPro.TMP_Text stoneText;
-    
+
 
     //Trajectory
     public GameObject point;
@@ -34,15 +34,19 @@ public class Slingshot : MonoBehaviour
     void Start()
     {
         points = new GameObject[numberOfPoints];
-        for(int i = 0; i < numberOfPoints; i++)
+        for (int i = 0; i < numberOfPoints; i++)
         {
             points[i] = Instantiate(point, projectilePoint.position, Quaternion.identity);
+
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        //UI
+        stoneText.text = "Stones: " + Stones;
+
 
         transform.position = Player.transform.position;
         //Slingshot rotation
@@ -51,25 +55,9 @@ public class Slingshot : MonoBehaviour
         direction = mousePos - slingshotPos;
         transform.right = direction;
 
-        //Force calculation     mouse distance = amount of force
-        //float Distance = Vector2.Distance(mousePos, slingshotPos);
-        //projectileForce = Distance * 2.25f;
-        //projectileForce = Mathf.Clamp(projectileForce, 5, 10);
-
-
-
-        //Shooting
-        //if (Input.GetMouseButtonDown(1))
-        //{
-        //    if(Stones > 0)
-        //    {
-        //        chargeTime = Time.time;
-        //        Debug.Log(chargeTime);
-        //    }
-        //}
 
         //Slingshot charge
-        if(Stones > 0)
+        if (Stones > 0)
         {
             if (Input.GetMouseButton(1))
             {
@@ -85,17 +73,18 @@ public class Slingshot : MonoBehaviour
             }
         }
 
-        
+
 
 
         //Trajectory
         for (int i = 0; i < numberOfPoints; i++)
         {
-            if(projectileForce > 0)
+            if (projectileForce > 0)
             {
                 points[i].gameObject.SetActive(true);
                 points[i].transform.position = PointPosition(i * spaceBetweenPoints);
-            }   else
+            }
+            else
             {
                 points[i].gameObject.SetActive(false);
             }
@@ -108,18 +97,11 @@ public class Slingshot : MonoBehaviour
         GameObject newProjectile = Instantiate(Projectile, projectilePoint.position, projectilePoint.rotation);
         newProjectile.GetComponent<Rigidbody2D>().velocity = transform.right * projectileForce;
         Stones--;
-        updateUI();
-
     }
 
     Vector2 PointPosition(float t)
     {
         Vector2 pos = (Vector2)projectilePoint.transform.position + (direction.normalized * projectileForce * t) + 0.5f * Physics2D.gravity * (t * t);
         return pos;
-    }
-
-    public void updateUI()
-    {
-        stoneText.text = "Stones: " + Stones;
     }
 }
