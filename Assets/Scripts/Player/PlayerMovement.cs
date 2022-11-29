1c41e6e29df7d47f539c8959363dcc1e72b220ea
Slowri <71567638+Slowri@users.noreply.github.com>
2022-11-29 08:22:15 +0100

    tset

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -75,7 +75,7 @@ public class PlayerMovement : MonoBehaviour
 
         if(Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.75f);
         }
 
     }
